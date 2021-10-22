0140a6f7bc6ef5e94ba86e381101f032a73fdb8f
Supun Arunoda <arunoda@exvision.co.jp>
2021-10-22 22:33:20 +0900

    Added Opencv test project

---
+++ OpenCvGuide/GuideOpencvV1.cpp
@@ -0,0 +1,18 @@
+#include "GuideOpencvV1.h"
+
+const int kNTrials = 10;
+
+
+void setValue() 
+{
+
+
+}
+
+
+int main(int argc, char** argv) {
+	if (argc != 4) {
+		std::cerr << "Incorrect Argument Passed!\n"
+			<< std::endl;
+	}
+}
---
+++ OpenCvGuide/GuideOpencvV1.h
@@ -0,0 +1,13 @@
+#ifndef OPENCV_GUIDE_V1_H_
+#define OPENCV_GUIDE_V1_H_
+
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+
+void setValue();
+
+#endif //OPENCV_GUIDE_V1_H_
